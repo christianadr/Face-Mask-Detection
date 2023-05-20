443588fb370401f2e85f5970198f29fd1ba5eacc
Christian Del Rosario <ainsley.drosario@gmail.com>
2023-05-20 21:25:24 +0800

    added assets and default image

--- app.py
+++ app.py
@@ -6,17 +6,13 @@ import cv2
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/best.pt')
 
 def detection(image):
-
     """ Run detection on uploaded image """
-
     results = model([image])
     return results
 
 def draw_bounding_boxes(image, boxes, labels, confidences):
-
     """ Drawing bounding boxes on detected objects on image 
         whether it has facemask or none """
-
     draw = ImageDraw.Draw(image)
     for box, label, confidence in zip(boxes, labels, confidences):
         color = 'red' if label == 'NO-Mask' else 'green'  # Use red for no_mask, green for mask
@@ -25,9 +21,7 @@ def draw_bounding_boxes(image, boxes, labels, confidences):
     return image
 
 def main():
-
     """ Displaying window using Streamlit API """
-
     st.header("Face Mask Detection using YOLOv5")
     st.write("""
     ##### Christian Ainsley Del Rosario | CPE32S6
@@ -35,17 +29,13 @@ def main():
     """)
 
     img = st.file_uploader("Choose an image from your computer", type=['jpg', 'png'])
-    
-    print(f'Uploading image...')
 
     col1, col2 = st.columns(2)
     if img is not None:
         with col1:
             image = Image.open(img)
             st.image(image, caption='Uploaded image', use_column_width=True)
-        
         with col2:
-
             image = Image.open(img)
             results = detection(image)
             boxes = results.pandas().xyxy[0][['xmin', 'ymin', 'xmax', 'ymax']].values.tolist()
@@ -56,10 +46,24 @@ def main():
             image_with_boxes = draw_bounding_boxes(image.copy(), boxes, labels, confidences)
             st.image(image_with_boxes, caption='With bounding boxes', use_column_width=True)
 
-        st.success("Image succesfully detected!")
+        st.success("Image successfully detected!")
     else:
-        st.write(" You can upload an image consisting of people with or without face mask")
-    
+        with col1:
+            default_image = Image.open('assets/download.jpg')  # Replace 'path/to/default_image.jpg' with the actual path to your default image
+            st.image(default_image, caption='Default Image', use_column_width=True)
+        with col2:
+            image = Image.open(default_image)
+            results = detection(image)
+            boxes = results.pandas().xyxy[0][['xmin', 'ymin', 'xmax', 'ymax']].values.tolist()
+            labels = results.pandas().xyxy[0]['name'].tolist()
+            confidences = results.pandas().xyxy[0]['confidence'].tolist()
+
+            # Display the image with bounding boxes and labels
+            image_with_boxes = draw_bounding_boxes(image.copy(), boxes, labels, confidences)
+            st.image(image_with_boxes, caption='With bounding boxes', use_column_width=True)
+
+        st.write("You can upload an image consisting of people with or without face mask")
+
     st.write(
         """
         The following link will redirect you to the Colab where the training of the model occurs: [Google Colab Link](https://colab.research.google.com/drive/1HzFrpWmx2o9AkPd9she6oArFm6cwN_ra?usp=sharing)
